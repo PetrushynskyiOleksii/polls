13f4abf8721e25be57b33eda4c28a0e24d524ba2
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-06 14:24:42 +0300

    Create List,Create api for question model

--- questions/urls.py
+++ questions/urls.py
@@ -1,9 +1,10 @@
 """Users' URL Configuration."""
 
-from django.conf.urls import url  # noqa: F401
-# from .views import QuestionRetrieveUpdateDestroyAPIView, CreateQuestionAPIView
+from django.conf.urls import url
+from .views import ListCreateQuestion
 
 urlpatterns = [
     # url(r'^(?P<pk>[0-9]+)/$', QuestionRetrieveUpdateDestroyAPIView.as_view()),
-    # url(r'^create/$', CreateQuestionAPIView.as_view()),
+    url(r'^create/$', ListCreateQuestion.as_view()),
+    url(r'^all/$', ListCreateQuestion.as_view()),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,29 @@
 """Views for questions' app."""
 
-from django.shortcuts import render  # noqa: F401
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class ListCreateQuestion(ListCreateAPIView):
+    """Create questions or show all existing questions."""
+
+    queryset = Question.objects.all()
+
+    def list(self, request):
+        """Represent all questions."""
+        queryset = self.get_queryset()
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """Create question."""
+        data = request.data
+        serializer = QuestionSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
