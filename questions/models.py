d8b052be4cd73967c0ca76e1c1d5051557afd0a7
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-06 11:49:26 +0300

    Create answer, question model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,45 @@
 """Models of questions' app."""
 
 from django.db import models  # noqa: F401
+
+
+class Question(models.Model):
+    """Model that represents question."""
+
+    class Meta:
+        """Meta data of question model."""
+
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+    question = models.CharField(verbose_name='Question',
+                                blank=True,
+                                max_length=255)
+    total_votes = models.IntegerField(verbose_name='Total Votes',
+                                      default=0)
+
+    def __str__(self):
+        """Render the question instance as a string."""
+        return self.question
+
+
+class Answer(models.Model):
+    """Model that represents answer."""
+
+    class Meta:
+        """Meta data of answer model."""
+
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
+
+    question = models.ForeignKey(Question,
+                                 verbose_name='Question')
+    answer = models.CharField(verbose_name='Answer',
+                              blank=True,
+                              max_length=255)
+    votes_count = models.IntegerField(verbose_name='Count of Votes',
+                                      default=0)
+
+    def __str__(self):
+        """Render the answer instance as a string."""
+        return ('%s - %d times') % (self.answer, self.votes_count)
