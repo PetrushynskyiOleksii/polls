8bf0f1276cc8423f536473de2f4e7df7ba0848d5
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-07 18:08:09 +0300

    Create views tests v1.1

--- questions/tests/test_views.py
+++ questions/tests/test_views.py
@@ -1,64 +1,90 @@
 """Test for views of question app."""
 
-from rest_framework import status
+import json
 
 from django.test import TestCase, Client
+
+from rest_framework import status
 
 from questions.models import Question, Answer
 
 
-class QuestionViewTest(TestCase):
-    """Test for post views."""
+class QuestionListViewTest(TestCase):
+    """Test for ListView of question."""
 
     def setUp(self):
         """Pre-populate test data."""
         self.client = Client()
         self.test_first_quest = Question.objects.create(question='test first question')
-        self.test_second_quest = Question.objects.create(question='test second question')
-
-        self.test_first_ans = Answer.objects.create(question=self.test_first_quest,
-                                                    answer='first test answer')
-        self.test_second_ans = Answer.objects.create(question=self.test_first_quest,
-                                                     answer='second test answer',
-                                                     votes_count=2)
-        self.test_third_ans = Answer.objects.create(question=self.test_first_quest,
-                                                    answer='third test answer',
-                                                    votes_count=3)
+        self.test_second_quest = Question.objects.create(question='test second question',
+                                                         total_votes=2)
+        self.test_third_quest = Question.objects.create(question='test third question',
+                                                        total_votes=3)
+        self.test_first_answer = Answer.objects.create(question=self.test_first_quest,
+                                                       answer='test first answer'
+                                                       )
 
     def tearDown(self):
         """Clean-up test data."""
         del self.client
         del self.test_first_quest
         del self.test_second_quest
-
-        del self.test_first_ans
-        del self.test_second_ans
-        del self.test_third_ans
+        del self.test_third_quest
+        del self.test_first_answer
 
     def test_questions_list(self):
         """Test list of questions."""
         response = self.client.get('/question/all/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data), Question.objects.count())
 
-    def test_question_crud(self):
+        response_json = json.dumps(response.data)
+        for quest in Question.objects.all():
+            self.assertIn(quest.question, response_json)
+            self.assertIn(str(quest.total_votes), response_json)
+            for ans in Answer.objects.filter(question=quest):
+                self.assertIn(ans.answer, response_json)
+                self.assertIn(str(ans.votes_count), response_json)
+
+
+class QuestionCRDViewTest(TestCase):
+    """Test for CRDView of question."""
+
+    def setUp(self):
+        """Pre-populate test data."""
+        self.client = Client()
+
+    def tearDown(self):
+        """Clean-up test data."""
+        del self.client
+
+    def test_question_create_delete_retrieve(self):
         """Test create question."""
-        data = {'question': 'test third question'}
+        data = {'question': 'test question'}
+
+        # ----- Create -------
         response = self.client.post('/question/create/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Question.objects.count(), 3)
-        self.assertEqual(Question.objects.get(id=3).question, 'test third question')
+        self.assertEqual(Question.objects.count(), 1)
+        quest = Question.objects.get(question='test question')
+        self.assertIsNotNone(quest)
+        self.assertEqual(quest.total_votes, 0)
 
-        response = self.client.get('/question/3/')
+        # ----- Retrieve -----
+        response = self.client.get('/question/{}/'.format(quest.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get('question'), 'test third question')
+        response_json = json.dumps(response.data)
+        self.assertTrue(quest.question in response_json)
+        self.assertTrue(str(quest.total_votes) in response_json)
 
         response = self.client.get('/question/100/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        response = self.client.delete('/question/3/')
+        # ----- Delete -------
+        response = self.client.delete('/question/{}/'.format(quest.id))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Question.objects.filter(id=3).first(), None)
+        with self.assertRaises(Question.DoesNotExist):
+            Question.objects.get(id=quest.id)
 
-        response = self.client.delete('question/3/')
+        response = self.client.delete('question/100/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- questions/urls.py
+++ questions/urls.py
@@ -1,10 +1,10 @@
 """Users' URL Configuration."""
 
 from django.conf.urls import url
-from .views import ListCreateQuestion, QuestionRetrieveUpdateDestroy
+from .views import ListCreateQuestion, QuestionRetrieveDestroy
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', QuestionRetrieveUpdateDestroy.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', QuestionRetrieveDestroy.as_view()),
     url(r'^create/$', ListCreateQuestion.as_view()),
     url(r'^all/$', ListCreateQuestion.as_view()),
 ]
