0a7a14af6798162b22e83ec8f9ca82183571adee
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-08 18:11:42 +0300

    Create update question view

--- questions/urls.py
+++ questions/urls.py
@@ -1,10 +1,10 @@
 """Users' URL Configuration."""
 
 from django.conf.urls import url
-from .views import ListCreateQuestion, QuestionRetrieveDestroy
+from .views import ListCreateQuestion, QuestionRetrieveUpdateDestroy
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', QuestionRetrieveDestroy.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', QuestionRetrieveUpdateDestroy.as_view()),
     url(r'^create/$', ListCreateQuestion.as_view()),
     url(r'^all/$', ListCreateQuestion.as_view()),
 ]
