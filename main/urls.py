a2e2187da1bb03ba0917577f3260aac294aa5f56
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-16 13:17:22 +0300

    Configure URLs for token, users' app

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework_jwt.views import (obtain_jwt_token,
+                                      verify_jwt_token,
+                                      refresh_jwt_token)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^question/', include('questions.urls', namespace='questions')),
+
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
+
+    url(r'^question/', include('questions.urls')),
+    url(r'^user/', include('users.urls')),
 ]
