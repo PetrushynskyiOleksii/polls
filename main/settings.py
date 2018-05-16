9d2212510c5b497a9cf7930c6c2a2a92dfe40273
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-16 13:16:50 +0300

    Install jwt and user app

--- main/settings.py
+++ main/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'questions',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -90,6 +91,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 
 # Internationalization
