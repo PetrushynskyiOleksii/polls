c47879e95e8c3d2606da18da5a3380a407468a54
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-06-20 17:22:46 +0300

    Create user profile model
      Manage admin panel for user profile

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 """Management admin panel of users' app."""
 
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.models import User
+
+from .models import UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    """Additional fields to Order on admin page."""
+
+    model = UserProfile
+    extra = 0
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    """Class that represents users at admin page."""
+
+    inlines = [UserProfileInline]
+    list_display = ('__str__', 'email', 'is_staff', 'is_active')
+    search_fields = ['__str__', 'email', 'is_staff', 'is_active']
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
 """Models of users' app."""
 
-from django.db import models  # noqa: F401
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from questions.models import Question
+
+
+class UserProfile(models.Model):
+    """Extra user data."""
+
+    class Meta(object):
+        """Meta data for user profile."""
+
+        verbose_name = u'User Profile'
+
+    user = models.OneToOneField(User)
+    voted_posts = models.ManyToManyField(Question,
+                                         verbose_name=u'Voted questions')
+
+    def __str__(self):
+        """Render the user instance as a string."""
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create user profile after create user."""
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Save user profile."""
+    instance.userprofile.save()
