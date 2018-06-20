6e5db7dd8e6ff00028a053675eef4ef7173b5bb4
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-06-20 17:20:40 +0300

    Add url for voting

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,11 @@
 from django.conf.urls import url
 
 from .views import (CreateUserView,
-                    LoginUserView)
+                    LoginUserView,
+                    votefor)
 
 urlpatterns = [
     url(r'^signup', CreateUserView.as_view()),
     url(r'^login', LoginUserView.as_view()),
+    url(r'^votefor/(?P<quest>[0-9]+)/answer/(?P<pk>[0-9]+)/$', votefor),
 ]
