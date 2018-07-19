5bb9be9d1153edc9788a1061caa495a21fb9aff2
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-07-19 19:45:02 +0300

    Update urls using routers

--- src/questions/urls.py
+++ src/questions/urls.py
@@ -1,15 +1,14 @@
 """Questions' URL Configuration."""
 
 from django.urls import path
-from .views import (QuestionList,
-                    QuestionCreate,
-                    QuestionRetrieveUpdateDestroy,
-                    votefor
-                    )
+from rest_framework import routers
+from .views import QuestionViewSet, votefor
+
+router = routers.SimpleRouter()
+router.register(r'', QuestionViewSet)
 
 urlpatterns = [
-    path('all/', QuestionList.as_view()),
-    path('create/', QuestionCreate.as_view()),
-    path('<int:pk>/', QuestionRetrieveUpdateDestroy.as_view()),
     path('<int:quest>/votefor/<int:pk>/', votefor),
 ]
+
+urlpatterns += router.urls
