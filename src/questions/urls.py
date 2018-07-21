86abc24d702bc9e354b102b6e7d499e8602b394d
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-07-21 20:34:35 +0300

    Create  views for answer instances
      - Retrieve, Update, Delete views.
      - IsAuthenticate, IsOwnerOrReadOnly permissions classes
      - qs - Answer.all()

--- src/questions/urls.py
+++ src/questions/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 from rest_framework import routers
-from .views import QuestionViewSet, votefor
+from .views import QuestionViewSet, votefor, AnswerViewSet
 
 router = routers.SimpleRouter()
 router.register(r'', QuestionViewSet)
+router.register(r'(?P<question_pk>\d+)', AnswerViewSet)
 
 urlpatterns = [
     path('<int:question_pk>/votefor/<int:answer_pk>/', votefor),
